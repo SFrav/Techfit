c46f8227aabdcc175a0f4979541007ed9ab30d05
John Mutua <jyumbya@yahoo.com>
2019-10-09 19:24:09 +0300

    removed setwd

--- PrepareBulkCropFAPAR.R
+++ PrepareBulkCropFAPAR.R
@@ -1,27 +1,29 @@
 library(raster)
-#library(sp)
 library(sf)
 library(rgdal)
 
-setwd("D:/GIS/Copernicus/fAPAR") #To location with fAPAR sub-folders
+iDir <-"D:/GIS/Copernicus/fAPAR" #To location with fAPAR sub-folders
 
 Maskshp <- readOGR("aoi.shp")
 
-# Getting the spatial extent of the shapefile
-e <- extent(Maskshp)
-
-for (i in 1:length(list.files(full.names=TRUE, pattern = ".nc", recursive = TRUE))) {
+for (i in 1:length(list.files(paste0(iDir, "/"), full.names = TRUE, pattern = ".nc", recursive = TRUE))) {
   
-  files <- list.files(full.names=TRUE, pattern = ".nc", recursive = TRUE)
+  EA <- paste0(iDir, "/EA/", sep = "")
+  if (!file.exists(EA)) {dir.create(EA, recursive = T)}
+  
+  files <- list.files(paste0(iDir, "/"), full.names = TRUE, pattern = ".nc", recursive = TRUE)
+  
   fAPAR_raster <- raster(files[i], varname = "FAPAR")
-  crs(fAPAR_raster) <- crs(Maskshp)
-
-  filename <- (paste("./EA/", tools::file_path_sans_ext(basename(files[i])), ".tif", sep=""))
   
-  # Crop the raster
-  fAPAR_raster.crop <- crop(fAPAR_raster, extent(Maskshp))
-  fAPAR_raster.crop <- mask(fAPAR_raster.crop, Maskshp)
-
-  #Export file 
-  writeRaster(Maskshp.masked, filename)
+  crs(fAPAR_raster) <- crs(Maskshp) #Copernicus fAPAR and DMP are in regular latitude/longitude grid WGS84
+  
+  filename <- paste("EA/", tools::file_path_sans_ext(basename(files[i])), ".tif", sep = "")
+  
+  # Crop and mask the raster
+  fAPAR_raster.masked <- mask(crop(fAPAR_raster, extent(Maskshp)), Maskshp)
+  
+  # Export file
+  writeRaster(fAPAR_raster.masked, paste(iDir, "/", filename, sep = ""), overwrite = TRUE)
+  
 }
+
